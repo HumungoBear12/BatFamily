a1924b11dd50eca7f0d7206cefe0de4b4fbb00df
Andrew Makris <amakri386@west-mec.org>
2022-01-19 20:12:28 -0700

    fixed font and changed layout

--- dc/src/Components/NavBar.jsx
+++ dc/src/Components/NavBar.jsx
@@ -11,13 +11,13 @@ const NavBar = () => {
     }
     return (
         <div>
-            <h1 className="nav">The Bat-Family</h1>
+            <h1 className="nav">The Bat Family</h1>
             <ul className='Navbar'>
                 <li className='active' onClick={() => {changeActive('home')}} id='home'><Link className='link' to='/'><h1>Home</h1></Link></li>
                 <li onClick={() => {changeActive('Characters')}} id='characters'><Link className='link' to='/characters'><h1>Characters</h1></Link></li>
                 <li onClick={() => {changeActive('Television')}} id='Television'><Link className='link' to='/Television'><h1>Television</h1></Link></li>
                 <li onClick={() => {changeActive('Movies')}} id='Movies'><Link className='link' to='/Movies'><h1>Movies</h1></Link></li>
-                <li onClick={() => {changeActive('VideoGames')}} id='VideoGames'><Link className='link' to='/VideoGames'><h1>VideoGames</h1></Link></li>
+                <li onClick={() => {changeActive('VideoGames')}} id='VideoGames'><Link className='link' to='/VideoGames'><h1>Video Games</h1></Link></li>
             </ul>
         </div>
     )
