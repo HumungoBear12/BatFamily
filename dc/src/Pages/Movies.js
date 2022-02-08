703ca7edf8244614c539db0d8b637de40be499cf
Amakris12 <amakri386@west-mec.org>
2022-02-08 12:59:21 -0700

    updating

--- dc/src/Pages/Movies.js
+++ dc/src/Pages/Movies.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Slideshow from '../Components/Slideshow'
 
 const Movies = () => {
-  return(
-      <div>
-        <h1>The Bat-Family in Movies</h1>
-        <Slideshow ImageOne="https://flxt.tmsimg.com/assets/p11724_p_v10_ac.jpg" ImageTwo="https://m.media-amazon.com/images/M/MV5BOGZmYzVkMmItM2NiOS00MDI3LWI4ZWQtMTg0YWZkODRkMmViXkEyXkFqcGdeQXVyODY0NzcxNw@@._V1_.jpg" ImageThree="https://m.media-amazon.com/images/M/MV5BNDdjYmFiYWEtYzBhZS00YTZkLWFlODgtY2I5MDE0NzZmMDljXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg" />
-      </div>
+  return (
+    <div>
+      <h1>The Bat-Family in Movies</h1>
+      {/* <Slideshow ImageOne="https://flxt.tmsimg.com/assets/p11724_p_v10_ac.jpg" ImageTwo="https://m.media-amazon.com/images/M/MV5BOGZmYzVkMmItM2NiOS00MDI3LWI4ZWQtMTg0YWZkODRkMmViXkEyXkFqcGdeQXVyODY0NzcxNw@@._V1_.jpg" ImageThree="https://m.media-amazon.com/images/M/MV5BNDdjYmFiYWEtYzBhZS00YTZkLWFlODgtY2I5MDE0NzZmMDljXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg" /> */}
+    </div>
   );
 };
 
