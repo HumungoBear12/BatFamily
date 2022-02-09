41f87d6bedbb1b1f75254b38f710a858c414d704
Amakris12 <amakri386@west-mec.org>
2022-02-09 13:00:39 -0700

    Movies and Games update

--- dc/src/Pages/Movies.js
+++ dc/src/Pages/Movies.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import Slideshow from '../Components/Slideshow'
 
 const Movies = () => {
   return (
     <div>
-      <h1>The Bat-Family in Movies</h1>
-      {/* <Slideshow ImageOne="https://flxt.tmsimg.com/assets/p11724_p_v10_ac.jpg" ImageTwo="https://m.media-amazon.com/images/M/MV5BOGZmYzVkMmItM2NiOS00MDI3LWI4ZWQtMTg0YWZkODRkMmViXkEyXkFqcGdeQXVyODY0NzcxNw@@._V1_.jpg" ImageThree="https://m.media-amazon.com/images/M/MV5BNDdjYmFiYWEtYzBhZS00YTZkLWFlODgtY2I5MDE0NzZmMDljXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg" /> */}
+      <h1 className="Movie-Title">The Bat-Family in Movies</h1>
+      
     </div>
   );
 };
--- dc/src/Pages/VideoGames.js
+++ dc/src/Pages/VideoGames.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import { React, useState} from 'react'
+import data from '../Util/Games'
 
 const VideoGames = () => {
+   const [people, setPeople] = useState(0)
+    const {rating,name,job,image,text} = data[people]
+    const checkNumber = (number) => {
+        if(number  > 3){
+            return 0;
+        }
+        if(number < 0){
+            return 3
+        }
+        return number
+    }
+    const next = () =>{
+        setPeople((people)=>{
+            let newPeople = people + 1;
+            return checkNumber(newPeople)
+        })
+    }
+   const back = () =>{
+        setPeople((people)=>{
+            let newPeople = people - 1;
+            return checkNumber(newPeople)
+        })
+    }
   return (
-  <div></div>
+  <div>
+      <h1>Our Reviews</h1>
+      <article className="main">
+        <img src={image} alt={name}/>
+        <h3>{name}</h3>
+        <p>{rating} stars</p>
+        <p>{job}</p>
+        <p>{text}</p>
+        <button onClick={next}>Next</button>
+        <button onClick={back}>Previous</button>
+    </article>
+  </div>
   )
 };
 
