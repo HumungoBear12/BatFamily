d5a538237eb6b528526cbedd5127b4b30279c4ee
Amakris12 <amakri386@west-mec.org>
2022-02-11 13:23:16 -0700

    Somewhat fixed the television page

--- dc/src/Pages/Television.js
+++ dc/src/Pages/Television.js
@@ -5,7 +5,9 @@ import TV from '../Util/TV';
 const Television = () => {
   const [televisions,setTelevisions] = useState(TV)
   console.log(televisions)
-  return <div className="TV-container">
+  return <div>
+    <h1 className="Television-Bat-Header">The Bat-Family on TV</h1>
+  <div className="TV-container">
     {televisions.map((tv)=>{
      const {id,Name,Year,Image} = tv;
      return (
@@ -13,6 +15,7 @@ const Television = () => {
         <div className="TV-Holder" key={id}>
           <img className="TV-image" src={Image} alt={Name}></img>
           <h1 className="TV-title">{Name}</h1>
+          <p className="TV-description">Description:</p>
           <p className="TV-Year">Years: {Year}</p>
         </div>
      </div>
@@ -21,7 +24,7 @@ const Television = () => {
 
   )}
   </div>
-   
+   </div>
 };
 
 export default Television;
