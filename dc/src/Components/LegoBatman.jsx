f2ac7f0e00a67486530831b21caf92771199174d
Andrew Makris <amakri386@west-mec.org>
2022-02-16 19:18:59 -0700

    Updating it for the night

--- dc/src/Components/LegoBatman.jsx
+++ dc/src/Components/LegoBatman.jsx
@@ -2,10 +2,10 @@ import {React, useState} from 'react'
 import data from '../Util/LegoBatman'
 
 const LegoBatman = () => {
-    const [games, setGames] = useState(0)
-    const {Name,Image,releaseDate,story,Creators} = data[games]
+    const [lego, setLego] = useState(0)
+    const {Name,Image,ReleaseDate,Story,Creators} = data[lego]
     const checkNumber = (number) => {
-        if(number  > 3){
+        if(number  > 2){
             return 0;
         }
         if(number < 0){
@@ -14,27 +14,27 @@ const LegoBatman = () => {
         return number
     }
     const next = () =>{
-        setGames((game)=>{
+        setLego((game)=>{
             let newGame = game + 1;
             return checkNumber(newGame)
         })
     }
    const back = () =>{
-        setGames((game)=>{
+        setLego((game)=>{
             let newGame = game - 1;
             return checkNumber(newGame)
         })
     }
   return (
         <div className="Games">
-            <h1>{games.length}</h1>
+            <h1>{lego.length}</h1>
             <article >
                 <img className="Game-IMG" src={Image} alt={Name}/>
                 <h3 className="Game-NAME">{Name}</h3>
-                <p className="Game-CREATORS">{Creators}: {releaseDate}</p>   
-                <p className="Game-DESCRIPTION">Story: {story}</p>
-                <button className="Game-NEXT" onClick={next}>Right</button>
-                <button className="Game-PREV" onClick={back}>Left</button>
+                <p className="Game-CREATORS">{Creators}: {ReleaseDate}</p>   
+                <p className="Game-DESCRIPTION">Story: {Story}</p>
+                <button className="Game-NEXT-Lego" onClick={next}>Right</button>
+                <button className="Game-PREV-Lego" onClick={back}>Left</button>
             </article>
         </div>
   )
